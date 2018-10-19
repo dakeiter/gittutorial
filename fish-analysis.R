efae4427b332746ef9c45076024ee7b8ec340dc9
David Keiter <dkeiter2@unl.edu>
2018-10-19 11:46:04 -0500

    Created histograms of scale length by length categories

--- fish-analysis.R
+++ fish-analysis.R
@@ -13,6 +13,10 @@ fishdat %>%
   mutate(length_cat=(case_when(length>=300 ~ "big",
                               length<300 ~ "small")))->fishdat
 
-
+ggplot(data = fishdat)+
+  geom_histogram(aes(x=scalelength, fill=length_cat))+
+  facet_wrap(~length_cat)+
+  theme_publication()
+  
 
 
