be1bf9294ad5876306075615131e9c3af8afb2fc
David Keiter <dkeiter2@unl.edu>
2018-10-19 11:33:31 -0500

    Changed cutoff length to 300

--- fish-analysis.R
+++ fish-analysis.R
@@ -8,12 +8,10 @@ source("R/themes.R")
 
 fishdat<-read_csv("C:/Users/dkeiter2/Desktop/Classes/DataMgmtVis/2018-10-05/gittutorial/data/Gaeta_etal_CLC_data.csv")
 
-fishdat %>% 
-  mutate(length_cat=(ifelse(length>=200, "big", "small")))->fishdat
 
 fishdat %>% 
-  mutate(length_cat=(case_when(length>=200 ~ "big",
-                              length<200 ~ "small")))->fishdat
+  mutate(length_cat=(case_when(length>=300 ~ "big",
+                              length<300 ~ "small")))->fishdat
 
 
 
