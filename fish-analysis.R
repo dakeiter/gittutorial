3d0518af2c766403ee38e0fa9d15167b5df373f7
Christopher Chizinski <chris.chizinski@gmail.com>
2018-10-23 14:06:21 -0500

    plot modification

--- fish-analysis.R
+++ fish-analysis.R
@@ -8,6 +8,8 @@ source("R/themes.R")
 
 fishdat<-read_csv("C:/Users/dkeiter2/Desktop/Classes/DataMgmtVis/2018-10-05/gittutorial/data/Gaeta_etal_CLC_data.csv")
 
+fishdat<-read_csv("data/Gaeta_etal_CLC_data.csv")
+
 
 fishdat %>% 
   mutate(length_cat=(case_when(length>=300 ~ "big",
@@ -19,4 +21,13 @@ ggplot(data = fishdat)+
   theme_publication()
   
 
+## Chris's modifications
+
+ggplot() +
+  geom_histogram(data = fishdat, aes(x = scalelength, fill = length_cat), colour = "black", bins = 25) +
+  facet_wrap(~length_cat) +
+  coord_cartesian(ylim = c(0,500), xlim = c(0, 12), expand = FALSE) +
+  theme_classic() +
+  theme(legend.position = "none",
+        panel.spacing = unit(2, "lines"))
 
